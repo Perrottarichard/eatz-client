b5b034c01cdf960b434df655343c8624bc004cba
Richard Perrotta <perrottarichard@gmail.com>
2020-10-19 17:16:57 +0700

    try without conditional redirect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import DashMui from './pages/DashMui'
 
@@ -13,18 +13,18 @@ function App() {
       <div className="App">
         <Switch>
           <Route path='/dashboard'>
-            {!user ? <Redirect to='/' />
+            {/* {!user ? <Redirect to='/' />
               :
-              <div>
-                <DashMui user={user} />
-              </div>
-            }
+              <div> */}
+            <DashMui user={user} />
+            {/* </div>
+            } */}
           </Route>
           <Route exact path='/'>
-            {user ? <Redirect to='/dashboard' />
-              :
-              <LandingPage />
-            }
+            {/* {user ? <Redirect to='/dashboard' />
+              : */}
+            <LandingPage />
+            {/* } */}
           </Route>
         </Switch>
       </div>
